8ea5701f8c55c05df2cd2c8b4a0d18345a9dd066
Dualock <djfonsecamo@gmail.com>
2022-09-04 02:14:32 -0600

    actualizacion lab1

--- 1Lab/bingo/bingo.asm
+++ 1Lab/bingo/bingo.asm
@@ -11,11 +11,8 @@
 ;--------------------------------------------------------
 ; external declarations
 ;--------------------------------------------------------
-	extern	__modsint
-	extern	__divsint
 	extern	_ANSEL
 	extern	_TRISIO
-	extern	_CMCON
 	extern	_GPIO
 	extern	_GPIObits
 	extern	__sdcc_gsinit_startup
@@ -23,6 +20,7 @@
 ; global declarations
 ;--------------------------------------------------------
 	global	_main
+	global	_LED_display_switching
 	global	_setup
 	global	_masking
 	global	_lfsr
@@ -73,16 +71,18 @@ STK00	res 1
 ; compiler-defined variables
 ;--------------------------------------------------------
 UDL_bingo_0	udata
+r0x1011	res	1
+r0x1012	res	1
+r0x1013	res	1
+r0x100C	res	1
+r0x100D	res	1
 r0x100E	res	1
 r0x100F	res	1
 r0x1010	res	1
-r0x1012	res	1
-r0x100C	res	1
-r0x100D	res	1
+r0x100B	res	1
 r0x1008	res	1
 r0x1009	res	1
 r0x100A	res	1
-r0x100B	res	1
 r0x1001	res	1
 r0x1000	res	1
 r0x1002	res	1
@@ -118,154 +118,119 @@ code_bingo	code
 ;has an exit
 ;functions called:
 ;   _setup
-;   _lfsr
-;   __divsint
-;   __modsint
 ;   _masking
 ;   _delay
-;   _masking
+;   _lfsr
+;   _lfsr
+;   _LED_display_switching
+;   _LED_display_switching
 ;   _delay
 ;   _setup
+;   _masking
+;   _delay
 ;   _lfsr
-;   __divsint
-;   __modsint
-;   _masking
+;   _lfsr
+;   _LED_display_switching
+;   _LED_display_switching
 ;   _delay
-;   _masking
-;   _delay
-;8 compiler assigned registers:
-;   r0x100E
-;   r0x100F
-;   r0x1010
-;   STK02
-;   STK01
-;   STK00
+;4 compiler assigned registers:
 ;   r0x1011
 ;   r0x1012
+;   r0x1013
+;   STK00
 ;; Starting pCode block
 S_bingo__main	code
 _main:
 ; 2 exit points
-;	.line	19; "bingo.c"	setup();
+;	.line	17; "bingo.c"	setup();
 	PAGESEL	_setup
 	CALL	_setup
 	PAGESEL	$
-_00113_DS_:
-;	.line	24; "bingo.c"	if(GP3){
+;	.line	20; "bingo.c"	unsigned char seed = 7;
+	MOVLW	0x07
+	MOVWF	r0x1011
+;	.line	24; "bingo.c"	while(1){
+	CLRF	r0x1012
+;;unsigned compare: left < lit(0x10=16), size=1
+_00114_DS_:
+;	.line	25; "bingo.c"	if(MAX_NUMBERS>counter){
+	MOVLW	0x10
+	SUBWF	r0x1012,W
+	BTFSC	STATUS,0
+	GOTO	_00111_DS_
+;;genSkipc:3307: created from rifx:0x7ffd74255670
+;	.line	26; "bingo.c"	GPIO |= masking(0);
+	MOVLW	0x00
+	PAGESEL	_masking
+	CALL	_masking
+	PAGESEL	$
+	MOVWF	r0x1013
+	BANKSEL	_GPIO
+	IORWF	_GPIO,F
+;	.line	27; "bingo.c"	delay(10); // Delay inicial
+	MOVLW	0x0a
+	MOVWF	STK00
+	MOVLW	0x00
+	PAGESEL	_delay
+	CALL	_delay
+	PAGESEL	$
+;	.line	28; "bingo.c"	if(PUSH_B){ // boton sin presionar
 	BANKSEL	_GPIObits
 	BTFSS	_GPIObits,3
-	GOTO	_00110_DS_
-;	.line	25; "bingo.c"	GPIO = 0x00;
+	GOTO	_00108_DS_
+;	.line	29; "bingo.c"	GPIO = 0x00;
 	CLRF	_GPIO
-	GOTO	_00113_DS_
-_00110_DS_:
-;	.line	28; "bingo.c"	seed = lfsr(seed);
-	MOVLW	0x29
+	GOTO	_00114_DS_
+_00108_DS_:
+;	.line	31; "bingo.c"	else if(!PUSH_B){ // boton presionado
+	BANKSEL	_GPIObits
+	BTFSC	_GPIObits,3
+	GOTO	_00114_DS_
+;	.line	32; "bingo.c"	seed = lfsr(seed);
+	MOVF	r0x1011,W
 	PAGESEL	_lfsr
 	CALL	_lfsr
 	PAGESEL	$
-;	.line	29; "bingo.c"	led1 = seed/10; 
-	MOVWF	r0x100E
-	MOVWF	r0x100F
-	CLRF	r0x1010
-	MOVLW	0x0a
-	MOVWF	STK02
-	MOVLW	0x00
-	MOVWF	STK01
-	MOVF	r0x100F,W
-	MOVWF	STK00
-	MOVLW	0x00
-	PAGESEL	__divsint
-	CALL	__divsint
-	PAGESEL	$
-;;1	MOVWF	r0x1011
-	MOVF	STK00,W
-	MOVWF	r0x100E
-	MOVWF	r0x1012
-;	.line	30; "bingo.c"	led2 = seed%10;		
-	MOVLW	0x0a
-	MOVWF	STK02
-	MOVLW	0x00
-	MOVWF	STK01
-	MOVF	r0x100F,W
-	MOVWF	STK00
-	MOVF	r0x1010,W
-	PAGESEL	__modsint
-	CALL	__modsint
-	PAGESEL	$
-	MOVWF	r0x100F
-	MOVF	STK00,W
-	MOVWF	r0x100E
-	MOVWF	r0x1010
-_00115_DS_:
-;	.line	32; "bingo.c"	if (!GP4){
-	BANKSEL	_GPIObits
-	BTFSC	_GPIObits,4
-	GOTO	_00106_DS_
-;	.line	33; "bingo.c"	mask = masking(led1);
+	MOVWF	r0x1011
+;	.line	34; "bingo.c"	led2 = seed - counter;
 	MOVF	r0x1012,W
-	PAGESEL	_masking
-	CALL	_masking
+	SUBWF	r0x1011,W
+;	.line	35; "bingo.c"	led2 = lfsr(led2);
+	MOVWF	r0x1013
+	PAGESEL	_lfsr
+	CALL	_lfsr
 	PAGESEL	$
-;	.line	34; "bingo.c"	GPIO |= mask;
-	MOVWF	r0x100E
-	BANKSEL	_GPIO
-	IORWF	_GPIO,F
-;	.line	35; "bingo.c"	GP4 = ~GP4;
-	CLRF	r0x100E
-	BTFSC	_GPIObits,4
-	INCF	r0x100E,F
-	COMF	r0x100E,W
-	MOVWF	r0x100E
-	RRF	r0x100E,W
-	BTFSS	STATUS,0
-	BCF	_GPIObits,4
-	BTFSC	STATUS,0
-	BSF	_GPIObits,4
-;	.line	36; "bingo.c"	delay(10);
-	MOVLW	0x0a
+;	.line	36; "bingo.c"	LED_display_switching(led1, led2);
+	MOVWF	r0x1013
 	MOVWF	STK00
-	MOVLW	0x00
-	PAGESEL	_delay
-	CALL	_delay
+	MOVF	r0x1011,W
+	PAGESEL	_LED_display_switching
+	CALL	_LED_display_switching
 	PAGESEL	$
-	GOTO	_00107_DS_
-_00106_DS_:
-;	.line	39; "bingo.c"	mask = masking(led2);
-	MOVF	r0x1010,W
-	PAGESEL	_masking
-	CALL	_masking
-	PAGESEL	$
-;	.line	40; "bingo.c"	GPIO |= mask;
-	MOVWF	r0x100E
-	BANKSEL	_GPIO
-	IORWF	_GPIO,F
-;	.line	41; "bingo.c"	GP4 = ~GP4;
-	CLRF	r0x100E
-	BTFSC	_GPIObits,4
-	INCF	r0x100E,F
-	COMF	r0x100E,W
-	MOVWF	r0x100E
-	RRF	r0x100E,W
-	BTFSS	STATUS,0
-	BCF	_GPIObits,4
-	BTFSC	STATUS,0
-	BSF	_GPIObits,4
-;	.line	42; "bingo.c"	delay(10);
-	MOVLW	0x0a
-	MOVWF	STK00
-	MOVLW	0x00
-	PAGESEL	_delay
-	CALL	_delay
-	PAGESEL	$
-_00107_DS_:
-;	.line	44; "bingo.c"	GPIO &= 0x10;
-	MOVLW	0x10
+;	.line	37; "bingo.c"	counter++; // por alguna razon este contador se resetea en 1	
+	INCF	r0x1012,F
+	GOTO	_00114_DS_
+_00111_DS_:
+;	.line	42; "bingo.c"	GPIO&=0x08; // limpia los bits menos GP3
+	MOVLW	0x08
 	BANKSEL	_GPIO
 	ANDWF	_GPIO,F
-;	.line	31; "bingo.c"	for(char i = 0; i<=255; i++){
-	GOTO	_00115_DS_
-;	.line	49; "bingo.c"	}
+;	.line	43; "bingo.c"	LED_display_switching(7,7);
+	MOVLW	0x07
+	MOVWF	STK00
+	MOVLW	0x07
+	PAGESEL	_LED_display_switching
+	CALL	_LED_display_switching
+	PAGESEL	$
+;	.line	44; "bingo.c"	delay(10);
+	MOVLW	0x0a
+	MOVWF	STK00
+	MOVLW	0x00
+	PAGESEL	_delay
+	CALL	_delay
+	PAGESEL	$
+	GOTO	_00114_DS_
+;	.line	47; "bingo.c"	}
 	RETURN	
 ; exit point of _main
 
@@ -287,30 +252,30 @@ _00107_DS_:
 S_bingo__delay	code
 _delay:
 ; 2 exit points
-;	.line	90; "bingo.c"	void delay(unsigned int tiempo)
+;	.line	102; "bingo.c"	void delay(unsigned int tiempo)
 	MOVWF	r0x1000
 	MOVF	STK00,W
 	MOVWF	r0x1001
-;	.line	95; "bingo.c"	for(i=0;i<tiempo;i++)
+;	.line	107; "bingo.c"	for(i=0;i<tiempo;(i)++)
 	CLRF	r0x1002
 	CLRF	r0x1003
-_00157_DS_:
+_00184_DS_:
 	MOVF	r0x1000,W
 	SUBWF	r0x1003,W
 	BTFSS	STATUS,2
-	GOTO	_00178_DS_
+	GOTO	_00205_DS_
 	MOVF	r0x1001,W
 	SUBWF	r0x1002,W
-_00178_DS_:
+_00205_DS_:
 	BTFSC	STATUS,0
-	GOTO	_00159_DS_
-;;genSkipc:3307: created from rifx:0x7ffdca9a32a0
-;	.line	96; "bingo.c"	for(j=0;j<1275;j++);
+	GOTO	_00186_DS_
+;;genSkipc:3307: created from rifx:0x7ffd74255670
+;	.line	108; "bingo.c"	for(j = 0; j<1275; (j)++);
 	MOVLW	0xfb
 	MOVWF	r0x1004
 	MOVLW	0x04
 	MOVWF	r0x1005
-_00155_DS_:
+_00182_DS_:
 	MOVLW	0xff
 	ADDWF	r0x1004,W
 	MOVWF	r0x1006
@@ -327,14 +292,14 @@ _00155_DS_:
 	MOVF	r0x1007,W
 	IORWF	r0x1006,W
 	BTFSS	STATUS,2
-	GOTO	_00155_DS_
-;	.line	95; "bingo.c"	for(i=0;i<tiempo;i++)
+	GOTO	_00182_DS_
+;	.line	107; "bingo.c"	for(i=0;i<tiempo;(i)++)
 	INCF	r0x1002,F
 	BTFSC	STATUS,2
 	INCF	r0x1003,F
-	GOTO	_00157_DS_
-_00159_DS_:
-;	.line	97; "bingo.c"	}
+	GOTO	_00184_DS_
+_00186_DS_:
+;	.line	112; "bingo.c"	}
 	RETURN	
 ; exit point of _delay
 
@@ -342,53 +307,40 @@ _00159_DS_:
 ;  pBlock Stats: dbName = C
 ;***
 ;has an exit
-;4 compiler assigned registers:
+;3 compiler assigned registers:
 ;   r0x1008
 ;   r0x1009
 ;   r0x100A
-;   r0x100B
 ;; Starting pCode block
 S_bingo__lfsr	code
 _lfsr:
 ; 2 exit points
-;	.line	72; "bingo.c"	unsigned char lfsr(unsigned char seed) {
+;	.line	82; "bingo.c"	unsigned char lfsr(unsigned char seed) {
 	MOVWF	r0x1008
-;	.line	74; "bingo.c"	feedback_bit = seed & PRIMITIVE; // aplicamos la mascara para obtener los bits a realimentar
-	MOVLW	0x52
+;	.line	84; "bingo.c"	feedback_bit = seed & PRIMITIVE; // aplicamos la mascara para obtener los bits a realimentar
+	MOVLW	0x03
 	ANDWF	r0x1008,W
 	MOVWF	r0x1009
-;	.line	75; "bingo.c"	feedback_bit = feedback_bit >> 6 ^feedback_bit >>4 ^ feedback_bit>>1;
-	SWAPF	r0x1009,W
-	ANDLW	0x0f
-	MOVWF	r0x100A
-;;shiftRight_Left2ResultLit:5474: shCount=1, size=1, sign=0, same=1, offr=0
-	BCF	STATUS,0
-	RRF	r0x100A,F
-;;shiftRight_Left2ResultLit:5474: shCount=1, size=1, sign=0, same=1, offr=0
-	BCF	STATUS,0
-	RRF	r0x100A,F
-	SWAPF	r0x1009,W
-	ANDLW	0x0f
-	MOVWF	r0x100B
-	XORWF	r0x100A,F
 ;;shiftRight_Left2ResultLit:5474: shCount=1, size=1, sign=0, same=0, offr=0
+;	.line	85; "bingo.c"	feedback_bit = feedback_bit >>1 ^ feedback_bit;
 	BCF	STATUS,0
 	RRF	r0x1009,W
-	MOVWF	r0x100B
+	MOVWF	r0x100A
+	MOVF	r0x1009,W
 	XORWF	r0x100A,F
-;	.line	76; "bingo.c"	feedback_bit &= 0x01; // Nos quedamos con el bit resultante de las xor anterior
+;	.line	86; "bingo.c"	feedback_bit &= 0x01;
 	MOVLW	0x01
 	ANDWF	r0x100A,F
 ;;shiftRight_Left2ResultLit:5474: shCount=1, size=1, sign=0, same=0, offr=0
-;	.line	77; "bingo.c"	seed>>=1; // hacemos el corrimiento
+;	.line	87; "bingo.c"	seed>>=1;
 	BCF	STATUS,0
 	RRF	r0x1008,W
 	MOVWF	r0x1009
-;	.line	78; "bingo.c"	seed|=(feedback_bit<<6); // realimentamos el bit resultante en el bit #6 del seed
+;	.line	88; "bingo.c"	seed|=(feedback_bit<<3);
 	MOVF	r0x100A,W
 	MOVWF	r0x1008
-	SWAPF	r0x1008,W
-	ANDLW	0xf0
+	BCF	STATUS,0
+	RLF	r0x1008,W
 	MOVWF	r0x100A
 	BCF	STATUS,0
 	RLF	r0x100A,F
@@ -398,28 +350,32 @@ _lfsr:
 	MOVWF	r0x1008
 	IORWF	r0x1009,W
 	MOVWF	r0x100A
-;	.line	79; "bingo.c"	seed &= 0x7F; // limpiamos el MSB para que no se pase de 100
-	MOVLW	0x7f
+;	.line	89; "bingo.c"	seed &= 0x0F; // limpiamos el MSB para que no se pase de 15
+	MOVLW	0x0f
 	ANDWF	r0x100A,W
 	MOVWF	r0x1008
-;;unsigned compare: left < lit(0x64=100), size=1
-;	.line	80; "bingo.c"	if(seed >= 100){
-	MOVLW	0x64
+;;unsigned compare: left < lit(0xA=10), size=1
+;	.line	91; "bingo.c"	if(seed >= 10){
+	MOVLW	0x0a
 	SUBWF	r0x1008,W
 	BTFSS	STATUS,0
-	GOTO	_00144_DS_
-;;genSkipc:3307: created from rifx:0x7ffdca9a32a0
+	GOTO	_00171_DS_
+;;genSkipc:3307: created from rifx:0x7ffd74255670
 ;;shiftRight_Left2ResultLit:5474: shCount=1, size=1, sign=0, same=0, offr=0
-;	.line	81; "bingo.c"	return seed>>=1; // se divide entre 2
+;	.line	92; "bingo.c"	seed = (seed>>1) - 1;
 	BCF	STATUS,0
 	RRF	r0x1008,W
 	MOVWF	r0x1009
-	GOTO	_00146_DS_
-_00144_DS_:
-;	.line	84; "bingo.c"	return seed;
+	MOVWF	r0x100A
+	DECF	r0x100A,W
+;	.line	93; "bingo.c"	return seed;
+	MOVWF	r0x1009
+	GOTO	_00173_DS_
+_00171_DS_:
+;	.line	96; "bingo.c"	return seed;
 	MOVF	r0x1008,W
-_00146_DS_:
-;	.line	86; "bingo.c"	}
+_00173_DS_:
+;	.line	98; "bingo.c"	}
 	RETURN	
 ; exit point of _lfsr
 
@@ -427,41 +383,36 @@ _00146_DS_:
 ;  pBlock Stats: dbName = C
 ;***
 ;has an exit
-;2 compiler assigned registers:
-;   r0x100C
-;   r0x100D
+;1 compiler assigned register :
+;   r0x100B
 ;; Starting pCode block
 S_bingo__masking	code
 _masking:
 ; 2 exit points
-;	.line	61; "bingo.c"	unsigned char masking(unsigned char number){
-	MOVWF	r0x100C
-;	.line	62; "bingo.c"	unsigned char mask = number;
-	MOVWF	r0x100D
-;	.line	63; "bingo.c"	switch(number){
-	MOVF	r0x100C,W
+;	.line	73; "bingo.c"	unsigned char masking(unsigned char number){
+	MOVWF	r0x100B
+;	.line	74; "bingo.c"	switch(number){
 	XORLW	0x08
 	BTFSC	STATUS,2
-	GOTO	_00124_DS_
-	MOVF	r0x100C,W
+	GOTO	_00150_DS_
+	MOVF	r0x100B,W
 	XORLW	0x09
 	BTFSC	STATUS,2
-	GOTO	_00125_DS_
-	GOTO	_00126_DS_
-_00124_DS_:
-;	.line	64; "bingo.c"	case 8: mask = 0x20;
+	GOTO	_00151_DS_
+	GOTO	_00152_DS_
+_00150_DS_:
+;	.line	75; "bingo.c"	case 8: return 0x20;
 	MOVLW	0x20
-	MOVWF	r0x100D
-;	.line	65; "bingo.c"	break;
-	GOTO	_00126_DS_
-_00125_DS_:
-;	.line	66; "bingo.c"	case 9: mask = 0x21;
+	GOTO	_00154_DS_
+_00151_DS_:
+;	.line	76; "bingo.c"	case 9: return 0x21;
 	MOVLW	0x21
-	MOVWF	r0x100D
-_00126_DS_:
-;	.line	69; "bingo.c"	return mask;
-	MOVF	r0x100D,W
-;	.line	70; "bingo.c"	}
+	GOTO	_00154_DS_
+_00152_DS_:
+;	.line	77; "bingo.c"	default: return number;
+	MOVF	r0x100B,W
+_00154_DS_:
+;	.line	80; "bingo.c"	}
 	RETURN	
 ; exit point of _masking
 
@@ -473,26 +424,128 @@ _00126_DS_:
 S_bingo__setup	code
 _setup:
 ; 2 exit points
-;	.line	52; "bingo.c"	TRISIO = 0b00001000; //Poner todos los pines como salidas
+;	.line	64; "bingo.c"	TRISIO = 0b00001000; //Poner todos los pines como salidas
 	MOVLW	0x08
 	BANKSEL	_TRISIO
 	MOVWF	_TRISIO
-;	.line	53; "bingo.c"	GPIO = 0x00; //Poner pines en bajo
+;	.line	65; "bingo.c"	GPIO = 0x00; //Poner pines en bajo
 	BANKSEL	_GPIO
 	CLRF	_GPIO
-;	.line	55; "bingo.c"	ANSEL = 0x00;
+;	.line	67; "bingo.c"	ANSEL = 0x00;
 	BANKSEL	_ANSEL
 	CLRF	_ANSEL
-;	.line	56; "bingo.c"	CMCON = 0x07;
-	MOVLW	0x07
-	BANKSEL	_CMCON
-	MOVWF	_CMCON
-;	.line	57; "bingo.c"	}
+;	.line	69; "bingo.c"	}
 	RETURN	
 ; exit point of _setup
 
+;***
+;  pBlock Stats: dbName = C
+;***
+;has an exit
+;functions called:
+;   _masking
+;   _delay
+;   _masking
+;   _delay
+;   _masking
+;   _delay
+;   _masking
+;   _delay
+;6 compiler assigned registers:
+;   r0x100C
+;   STK00
+;   r0x100D
+;   r0x100E
+;   r0x100F
+;   r0x1010
+;; Starting pCode block
+S_bingo__LED_display_switching	code
+_LED_display_switching:
+; 2 exit points
+;	.line	48; "bingo.c"	void LED_display_switching(unsigned char led1, unsigned char led2){
+	MOVWF	r0x100C
+	MOVF	STK00,W
+	MOVWF	r0x100D
+;	.line	49; "bingo.c"	for(int k = 0; k<100; k++){
+	CLRF	r0x100E
+	CLRF	r0x100F
+;;signed compare: left < lit(0x64=100), size=2, mask=ffff
+_00124_DS_:
+	MOVF	r0x100F,W
+	ADDLW	0x80
+	ADDLW	0x80
+	BTFSS	STATUS,2
+	GOTO	_00141_DS_
+	MOVLW	0x64
+	SUBWF	r0x100E,W
+_00141_DS_:
+	BTFSC	STATUS,0
+	GOTO	_00126_DS_
+;;genSkipc:3307: created from rifx:0x7ffd74255670
+;	.line	50; "bingo.c"	if (SELECTOR){ //Display Unidades
+	BANKSEL	_GPIObits
+	BTFSS	_GPIObits,4
+	GOTO	_00120_DS_
+;	.line	51; "bingo.c"	GPIO |= masking(led1);				
+	MOVF	r0x100C,W
+	PAGESEL	_masking
+	CALL	_masking
+	PAGESEL	$
+	MOVWF	r0x1010
+	BANKSEL	_GPIO
+	IORWF	_GPIO,F
+;	.line	52; "bingo.c"	delay(10);
+	MOVLW	0x0a
+	MOVWF	STK00
+	MOVLW	0x00
+	PAGESEL	_delay
+	CALL	_delay
+	PAGESEL	$
+	GOTO	_00121_DS_
+_00120_DS_:
+;	.line	55; "bingo.c"	GPIO |= masking(led2);
+	MOVF	r0x100D,W
+	PAGESEL	_masking
+	CALL	_masking
+	PAGESEL	$
+	MOVWF	r0x1010
+	BANKSEL	_GPIO
+	IORWF	_GPIO,F
+;	.line	56; "bingo.c"	delay(10);
+	MOVLW	0x0a
+	MOVWF	STK00
+	MOVLW	0x00
+	PAGESEL	_delay
+	CALL	_delay
+	PAGESEL	$
+_00121_DS_:
+;	.line	58; "bingo.c"	GP4 = ~GP4;
+	CLRF	r0x1010
+	BANKSEL	_GPIObits
+	BTFSC	_GPIObits,4
+	INCF	r0x1010,F
+	COMF	r0x1010,W
+	MOVWF	r0x1010
+	RRF	r0x1010,W
+	BTFSS	STATUS,0
+	BCF	_GPIObits,4
+	BTFSC	STATUS,0
+	BSF	_GPIObits,4
+;	.line	59; "bingo.c"	GPIO &= 0x18; // limpia los bits menos GP3 y GP4
+	MOVLW	0x18
+	ANDWF	_GPIO,F
+;	.line	49; "bingo.c"	for(int k = 0; k<100; k++){
+	INCF	r0x100E,F
+	BTFSC	STATUS,2
+	INCF	r0x100F,F
+	GOTO	_00124_DS_
+_00126_DS_:
+;	.line	61; "bingo.c"	}
+	RETURN	
+; exit point of _LED_display_switching
+
 
 ;	code size estimation:
-;	  190+   25 =   215 instructions (  480 byte)
+;	  199+   35 =   234 instructions (  538 byte)
 
 	end
--- 1Lab/bingo/bingo.c
+++ 1Lab/bingo/bingo.c
@@ -1,50 +1,62 @@
 #include <pic14/pic12f675.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define PRIMITIVE 0x52 // mascara para elegir que bits realimentar en este caso 6 xor 4 xor 2
-
+#define PRIMITIVE 0x03 // mascara para elegir que bits realimentar en este caso 3 xor 1
+#define PUSH_B GP3
+#define SELECTOR GP4
+#define MAX_NUMBERS 16
 void delay (unsigned int tiempo);
 void setup();
 unsigned char masking(unsigned char number);
 unsigned char lfsr(unsigned char seed);
-unsigned char get_unit(unsigned char number);
-
+void LED_display_switching(unsigned char led1, unsigned char led2);
 
 void main(void)
 {
-   	const unsigned int time = 10000;
-   	unsigned char mask;
-    	unsigned char seed = 41;
+	setup();
+	const unsigned int time = 10000;
+	unsigned char counter = 0;
+	unsigned char seed = 7;
+   	//delay(10);
 	unsigned char led1;
 	unsigned char led2;
-   	setup();
-    //Loop forever
-	while ( 1 )
-    	{
-    		//GP1 = 0x00;
-    		if(GP3){
-    			GPIO = 0x00;
-    		}
-    		else{
-			seed = lfsr(seed);
-			led1 = seed/10; 
-			led2 = seed%10;		
-			for(char i = 0; i<=255; i++){
-				if (!GP4){
-	    				mask = masking(led1);
-	    				GPIO |= mask;
-					GP4 = ~GP4;
-					delay(10);
-				}
-				else{
-					mask = masking(led2);
-	    				GPIO |= mask;
-					GP4 = ~GP4;
-					delay(10);
+    	while(1){
+	    	if(MAX_NUMBERS>counter){
+	    		GPIO |= masking(0); // Se pone en 0 
+	    		delay(10); // Delay inicial
+		    	if(PUSH_B){ // boton sin presionar
+		    		GPIO = 0x00;
+		    	}
+		    	else if(!PUSH_B){ // boton presionado
+				seed = lfsr(seed);
+				led1 = seed;
+				led2 = seed - counter;
+				led2 = lfsr(led2);
+				LED_display_switching(led1, led2);
+				counter++; // por alguna razon este contador se resetea en 1	
 			}
-			GPIO &= 0x10;
-			//delay(time);
-			}		
 		}
+		// si ya pasaron los 16 numeros
+		else{
+		GPIO&=0x08; // limpia los bits menos GP3
+		LED_display_switching(7,7);
+		delay(10);
+		}
+	}
+}
+void LED_display_switching(unsigned char led1, unsigned char led2){
+	for(int k = 0; k<100; k++){
+        	if (SELECTOR){ //Display Unidades
+			GPIO |= masking(led1);				
+			delay(10);
+		}
+		else{ // Display Decenas
+			GPIO |= masking(led2);
+			delay(10);
+		}
+		GP4 = ~GP4;
+		GPIO &= 0x18; // limpia los bits menos GP3 y GP4
 	}
 }
 void setup(){
@@ -53,32 +65,32 @@ void setup(){
 	GPIO = 0x00; //Poner pines en bajo
 	word __at 0x207 __CONFIG = (_WDTE_OFF & _WDT_OFF & _MCLRE_OFF);
 	ANSEL = 0x00;
-	CMCON = 0x07;
+	//CMCON = 0x07;
 }
 
 // Esta funcion retorna una mascara para activar los bits del GPIO, del 0 al 7 la mascara es el mismo numero
 //unicamente cambia en los numeros 8 y 9 ya que el 4 bit es la entrada
 unsigned char masking(unsigned char number){
-	unsigned char mask = number;
 	switch(number){
-		case 8: mask = 0x20;
-		break;
-		case 9: mask = 0x21;
-		break;
+		case 8: return 0x20;
+		case 9: return 0x21;
+		default: return number;
 	}
-	return mask;
+	//return mask;
 }
 
 unsigned char lfsr(unsigned char seed) {
 	unsigned char feedback_bit;
 	feedback_bit = seed & PRIMITIVE; // aplicamos la mascara para obtener los bits a realimentar
-	feedback_bit = feedback_bit >> 6 ^feedback_bit >>4 ^ feedback_bit>>1;
-	feedback_bit &= 0x01; // Nos quedamos con el bit resultante de las xor anterior
-	seed>>=1; // hacemos el corrimiento
-	seed|=(feedback_bit<<6); // realimentamos el bit resultante en el bit #6 del seed
-	seed &= 0x7F; // limpiamos el MSB para que no se pase de 100
-	if(seed >= 100){
-		return seed>>=1; // se divide entre 2
+	feedback_bit = feedback_bit >>1 ^ feedback_bit;
+	feedback_bit &= 0x01;
+	seed>>=1;
+	seed|=(feedback_bit<<3);
+	seed &= 0x0F; // limpiamos el MSB para que no se pase de 15
+	
+	if(seed >= 10){
+		seed = (seed>>1) - 1;
+		return seed;
 	}
 	else{
 		return seed;
@@ -91,9 +103,12 @@ void delay(unsigned int tiempo)
 {
 	unsigned int i;
 	unsigned int j;
+	//*i = 0;
+	for(i=0;i<tiempo;(i)++)
+	for(j = 0; j<1275; (j)++);
 
-	for(i=0;i<tiempo;i++)
-	  for(j=0;j<1275;j++);
+
+
 }
 
 
